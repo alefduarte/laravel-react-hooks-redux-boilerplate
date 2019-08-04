a9d7ed8ee19e7f6ceb5b4ddf3a2d87e2a069e830
Alef Duarte <alef.duarte@outlook.com>
2019-08-03 22:36:16 -0300

    Added user sagas and types

--- resources/js/store/sagas/index.js
+++ resources/js/store/sagas/index.js
@@ -3,10 +3,12 @@ import { takeLatest, all } from 'redux-saga/effects';
 /* ---------------------- Types --------------------- */
 
 import { Types as AuthTypes } from '../ducks/auth';
+import { Types as UsersTypes } from '../ducks/users';
 
 /* ---------------------- Sagas --------------------- */
 
 import { loginRequest, logoutRequest } from './auth';
+import { indexUsers, storeUsers, destroyUsers } from './users';
 
 /* ------------- Connect Types To Sagas ------------- */
 
@@ -15,5 +17,8 @@ export default function* root() {
         /* ----------------------- Auth --------------------- */
         takeLatest(AuthTypes.LOGIN_REQUEST, loginRequest),
         takeLatest(AuthTypes.LOGOUT_REQUEST, logoutRequest),
+        takeLatest(UsersTypes.INDEX_USERS, indexUsers),
+        takeLatest(UsersTypes.STORE_USER, storeUsers),
+        takeLatest(UsersTypes.DESTROY_USER, destroyUsers),
     ]);
 }
