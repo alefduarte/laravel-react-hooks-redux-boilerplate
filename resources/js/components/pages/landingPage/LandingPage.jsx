9b2b6cc65e09d1f1022195e5447bc4c590fdf8b6
Alef Duarte <alef.duarte@outlook.com>
2019-08-04 20:16:33 -0300

    Treated when user is logged in nd otherwise

--- resources/js/components/pages/landingPage/LandingPage.jsx
+++ resources/js/components/pages/landingPage/LandingPage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { Result, Icon } from "antd";
+import { useSelector } from "react-redux";
 import { Trans, useTranslation } from "react-i18next";
+import { Result, Icon } from "antd";
+import { isLoggedIn } from "@ducks/auth";
 
 const LandingPage = () => {
     const { t } = useTranslation();
+    const isUserAuthenticated = useSelector(state => isLoggedIn(state));
+    const username = useSelector(state =>
+        state.auth.user ? state.auth.user.name : ""
+    );
+
     return (
         <Result
             className="pulseIcon"
@@ -16,15 +23,34 @@ const LandingPage = () => {
             }
             title={
                 <>
-                    <Trans i18nKey="landingPage.notLoggedIn">
-                        {t("landingPage.notLoggedIn")}
-                    </Trans>
-                    <Icon
-                        style={{ paddingLeft: "5px" }}
-                        type="exclamation-circle"
-                        theme="twoTone"
-                        twoToneColor="#ff935c"
-                    />
+                    {t("landingPage.welcome")}
+                    <br />
+                    {isUserAuthenticated ? (
+                        <>
+                            <Trans
+                                i18nKey="landingPage.loggedIn"
+                                values={{ name: username }}
+                            >
+                                {t("landingPage.loggedIn")}
+                            </Trans>
+                            <Icon
+                                style={{ paddingLeft: "5px" }}
+                                type="check-circle"
+                                theme="twoTone"
+                                twoToneColor="#52c41a"
+                            />
+                        </>
+                    ) : (
+                        <>
+                            {t("landingPage.notLoggedIn")}
+                            <Icon
+                                style={{ paddingLeft: "5px" }}
+                                type="exclamation-circle"
+                                theme="twoTone"
+                                twoToneColor="#ff935c"
+                            />
+                        </>
+                    )}
                 </>
             }
         />
