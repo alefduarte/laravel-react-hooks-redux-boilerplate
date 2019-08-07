48b789a373519cc837d807e342f24eb716a30f3f
Alef Duarte <alef.duarte@outlook.com>
2019-08-07 06:38:49 -0300

    Fixed resetNotification Path

--- app/Notifications/PasswordResetRequest.php
+++ app/Notifications/PasswordResetRequest.php
@@ -41,7 +41,7 @@ class PasswordResetRequest extends Notification implements ShouldQueue
      */
     public function toMail($notifiable)
     {
-        $url = url('/api/password/find/' . $this->token);
+        $url = url('/reset/' . $this->token);
 
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
