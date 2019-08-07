1afe9b499c76fd184a3266c28b4f858ae4315d1b
Alef Duarte <alef.duarte@outlook.com>
2019-08-07 06:42:05 -0300

    exported password reducer and saga

--- resources/js/store/ducks/index.js
+++ resources/js/store/ducks/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import users from './users';
+import password from './password';
 
 
 export default combineReducers({
   auth,
-  users
+  users,
+  password
 });
--- resources/js/store/sagas/index.js
+++ resources/js/store/sagas/index.js
@@ -3,11 +3,13 @@ import { takeLatest, all } from 'redux-saga/effects';
 /* ---------------------- Types --------------------- */
 
 import { Types as AuthTypes } from '../ducks/auth';
+import { Types as PasswordTypes } from '../ducks/password';
 import { Types as UsersTypes } from '../ducks/users';
 
 /* ---------------------- Sagas --------------------- */
 
 import { loginRequest, logoutRequest } from './auth';
+import { tokenRequest, passwordRequest } from './password';
 import { indexUsers, storeUsers, destroyUsers } from './users';
 
 /* ------------- Connect Types To Sagas ------------- */
@@ -17,8 +19,13 @@ export default function* root() {
         /* ----------------------- Auth --------------------- */
         takeLatest(AuthTypes.LOGIN_REQUEST, loginRequest),
         takeLatest(AuthTypes.LOGOUT_REQUEST, logoutRequest),
+        /* ----------------------- Users -------------------- */
         takeLatest(UsersTypes.INDEX_USERS, indexUsers),
         takeLatest(UsersTypes.STORE_USER, storeUsers),
         takeLatest(UsersTypes.DESTROY_USER, destroyUsers),
+        /* ----------------------- Password ----------------- */
+        takeLatest(PasswordTypes.TOKEN_REQUEST, tokenRequest),
+        takeLatest(PasswordTypes.RESET_REQUEST, passwordRequest),
+
     ]);
 }
