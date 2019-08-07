d0536a2379d9bab8884bb54a9c800cbddec1df86
Alef Duarte <alef.duarte@outlook.com>
2019-08-07 06:47:22 -0300

    added isAdmin middleware

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = Auth::user();
+
+        if ($user->type == 'admin') {
+            return $next($request);
+        }
+        return response('Unauthorized.', 401); // If user is not an admin.
+    }
+}
