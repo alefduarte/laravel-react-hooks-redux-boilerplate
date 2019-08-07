e57c2f1c62421a1fae9465771fe38f2f7e74b86e
Alef Duarte <alef.duarte@outlook.com>
2019-08-07 06:42:38 -0300

    added reset password routes

--- resources/js/routes/Routes.js
+++ resources/js/routes/Routes.js
@@ -7,13 +7,27 @@ import {
 import PublicRoute from './PublicRoute';
 import PrivateRoute from './PrivateRoute';
 import history from './history'
-import { Login, Register, Logout, NotFound, LandingPage, HomePage, Dashboard } from '@pages';
+import {
+    Login,
+    Register,
+    Logout,
+    NotFound,
+    LandingPage,
+    HomePage,
+    Dashboard,
+    LostPasswordPage,
+    ResetPasswordPage
+} from '@pages';
 
 const Routes = () => (
     <Router history={history}>
         <Switch>
             <PublicRoute restricted={false} component={LandingPage} path="/" exact />
+            <PublicRoute restricted={false} component={LostPasswordPage} path="/lostpass" exact />
+            <PublicRoute restricted={false} component={LostPasswordPage} path="/lostpass/:slug" exact />
+            <PublicRoute restricted={false} component={ResetPasswordPage} path="/reset/:slug" exact />
             <PublicRoute restricted component={Login} path="/login" exact />
+            <PublicRoute restricted component={Login} path="/login/:slug" exact />
             <PrivateRoute component={Logout} path="/logout" exact />
             <PublicRoute restricted component={Register} path="/register" exact />
             <PrivateRoute component={HomePage} path="/home" exact />
