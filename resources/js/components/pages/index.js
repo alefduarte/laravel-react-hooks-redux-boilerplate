f834b7243c7d304ffb326024d6eb4a06e660d418
Alef Duarte <alef.duarte@outlook.com>
2019-08-07 06:46:08 -0300

    added lostpassword export

--- resources/js/components/pages/index.js
+++ resources/js/components/pages/index.js
@@ -5,3 +5,5 @@ export { default as Register } from './register/Register';
 export { default as LandingPage } from './landingPage/LandingPage';
 export { default as HomePage } from './home/HomePage';
 export { default as Dashboard } from './dashboard/Dashboard';
+export { default as LostPasswordPage } from './resetPassword/LostPasswordPage';
+export { default as ResetPasswordPage } from './resetPassword/ResetPasswordPage';
