4a7b37c60a654659e509002a351404b3d0a01b58
Alef Duarte <alef.duarte@outlook.com>
2019-08-09 20:53:08 -0300

    Created activate account page

---
+++ resources/js/pages/activateAccount/ActivateAccount.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { Result, Icon, Button, Spin, Typography } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import { useTranslation } from "react-i18next";
+import { isError, isActive, Types } from "@ducks/activate";
+import { Types as AuthTypes, isLoggedIn } from "@ducks/auth";
+import history from "@routes/history";
+
+const { Paragraph, Text } = Typography;
+
+function ActivateAccount({
+    match: {
+        params: { slug }
+    }
+}) {
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const isAuthenticated = useSelector(state => isLoggedIn(state));
+    const isFetching = useSelector(state => state.activate.fetching);
+    const error = useSelector(state => isError(state));
+    const active = useSelector(state => isActive(state));
+
+    useEffect(() => {
+        dispatch({ type: Types.ACTIVATE_REQUEST, token: slug });
+    }, []);
+
+    useEffect(() => {
+        if (active && isAuthenticated) {
+            dispatch({ type: AuthTypes.REFRESH_REQUEST });
+        }
+    }, [active]);
+
+    return (
+        <Spin spinning={isFetching}>
+            {error && (
+                <Result
+                    status="error"
+                    title={t("activation.failedTitle")}
+                    subTitle={
+                        <Paragraph>
+                            <Text
+                                strong
+                                style={{
+                                    fontSize: 16
+                                }}
+                            >
+                                {t("activation.the_activation_code_has_either")}
+                                <font color="red">
+                                    {t("activation.expired")}
+                                </font>
+                                {t("activation.or_is")}
+                                <font color="red">
+                                    {t("activation.invalid")}
+                                </font>
+                                <br />
+                                {t("activation.try_resending_activation_code")}
+                            </Text>
+                        </Paragraph>
+                    }
+                    extra={[
+                        <Button
+                            type="primary"
+                            key="resend"
+                            onClick={() => history.push("/home")}
+                        >
+                            {t("activation.failedButton")}
+                        </Button>
+                    ]}
+                />
+            )}
+            {active && (
+                <Result
+                    icon={<Icon type="smile" theme="twoTone" />}
+                    title={t("activation.successTitle")}
+                    extra={
+                        <Button
+                            type="primary"
+                            onClick={() => history.push("/home")}
+                        >
+                            {t("activation.home")}
+                        </Button>
+                    }
+                />
+            )}
+        </Spin>
+    );
+}
+
+ActivateAccount.propTypes = {
+    match: PropTypes.shape({
+        params: PropTypes.shape({
+            slug: PropTypes.string
+        })
+    }).isRequired
+};
+
+export default ActivateAccount;
