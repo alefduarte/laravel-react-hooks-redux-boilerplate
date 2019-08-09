1c011ef4138b2f44c5c7578ea3bb94faaf10d169
Alef Duarte <alef.duarte@outlook.com>
2019-08-09 20:55:59 -0300

    added activate saga to combineReducer

--- resources/js/store/ducks/index.js
+++ resources/js/store/ducks/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import users from './users';
 import password from './password';
+import activate from './activate';
 
 
 export default combineReducers({
   auth,
   users,
-  password
+  password,
+  activate
 });
