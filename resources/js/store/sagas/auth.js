835d86fca4550a89e68ecd2f6c6447b75c5c6cf4
Alef Duarte <alef.duarte@outlook.com>
2019-08-10 23:36:37 -0300

    added refreshRequest function and improved login to handle unauthorized login

--- resources/js/store/sagas/auth.js
+++ resources/js/store/sagas/auth.js
@@ -31,6 +31,17 @@ export function* loginRequest({ email, password, remember_me }) {
   }
 }
 
+export function* refreshRequest() {
+  try {
+    const user = yield call(() => getUser());
+    yield LocalStorage.setItem('user', user.data);
+
+    yield put(Creators.refreshSuccess(user.data));
+  } catch (error) {
+    yield put(Creators.loginFailure(error.toString()));
+  }
+}
+
 export function* refreshTokenRequest() {
   try {
     // Implementar logica para verificar a validade do token antes de solicitar um novo.
@@ -53,10 +64,16 @@ export function* refreshTokenRequest() {
  *  chamar o método para revogar o token de acesso antes
  *  de remover as informações do armazenamento local. */
 export function* logoutRequest() {
-  const user = yield call(() => getUser());
-  yield call(() => logout(user));
-  yield call(() => LocalStorage.clear());
-
-  // Redireciona pra a pagina de login.
-  yield history.push('/login');
-}
+  try {
+    const user = yield call(() => getUser());
+    yield call(() => logout(user));
+    yield call(() => LocalStorage.clear());
+    yield history.push('/login');
+  }
+  catch (error) {
+    if (error.response.status === 401) {
+      yield call(() => LocalStorage.clear());
+      yield history.push('/login');
+    }
+  }
+} 
