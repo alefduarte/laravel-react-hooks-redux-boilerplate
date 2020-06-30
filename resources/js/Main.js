59b85e42336182e3c1e7b6b68ff92b7b67fefdbc
Alef Duarte <alef.duarte@outlook.com>
2020-06-30 18:30:27 -0300

    changed LocaleProvider to ConfigProvider due to antd update

--- resources/js/Main.js
+++ resources/js/Main.js
@@ -1,26 +1,32 @@
 import React, { Suspense } from 'react';
-import PropTypes from "prop-types";
-import { LocaleProvider, Layout, Spin } from 'antd';
+import PropTypes from 'prop-types';
+import { ConfigProvider, Layout, Spin } from 'antd';
 import ptBR from 'antd/es/locale-provider/pt_BR';
 
 const Main = ({ children }) => {
     return (
-        <LocaleProvider locale={ptBR}>
-            <Suspense fallback={<Spin style={{
-                margin: "auto",
-                width: "100%",
-                position: "absolute",
-                top: "50%"
-            }} />}>
-                <Layout style={{ background: "#fff", padding: 0 }}>
+        <ConfigProvider locale={ptBR}>
+            <Suspense
+                fallback={
+                    <Spin
+                        style={{
+                            margin: 'auto',
+                            width: '100%',
+                            position: 'absolute',
+                            top: '50%',
+                        }}
+                    />
+                }
+            >
+                <Layout style={{ background: '#fff', padding: 0 }}>
                     {children}
                 </Layout>
             </Suspense>
-        </LocaleProvider>
-    )
-}
+        </ConfigProvider>
+    );
+};
 
 Main.propTypes = {
-    children: PropTypes.arrayOf(PropTypes.object).isRequired
+    children: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 export default Main;
