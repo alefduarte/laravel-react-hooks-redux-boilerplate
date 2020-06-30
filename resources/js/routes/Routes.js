f4166435296db7cbeee31607f82b614d9bc91b05
Alef Duarte <alef.duarte@outlook.com>
2020-06-30 18:32:07 -0300

    updated path property with nullable slug and formatted document. Changed NotFound component to use PublicRoute

--- resources/js/routes/Routes.js
+++ resources/js/routes/Routes.js
@@ -1,12 +1,5 @@
-import React from 'react'
-import {
-    Router,
-    Switch,
-    Route
-} from 'react-router-dom'
-import PublicRoute from './PublicRoute';
-import PrivateRoute from './PrivateRoute';
-import history from './history'
+import React from 'react';
+import { Router, Switch } from 'react-router-dom';
 import {
     Login,
     Register,
@@ -18,26 +11,62 @@ import {
     LostPasswordPage,
     ResetPasswordPage,
     ActivateAccount,
-    SendActivationPage
+    SendActivationPage,
 } from '@pages';
+import PublicRoute from './PublicRoute';
+import PrivateRoute from './PrivateRoute';
+import history from './history';
 
 const Routes = () => (
     <Router history={history}>
         <Switch>
-            <PublicRoute restricted={false} component={LandingPage} path="/" exact />
-            <PublicRoute restricted={false} component={LostPasswordPage} path="/lostpass" exact />
-            <PublicRoute restricted={false} component={LostPasswordPage} path="/lostpass/:slug" exact />
-            <PublicRoute restricted={false} component={ResetPasswordPage} path="/reset/:slug" exact />
-            <PublicRoute restricted={false} component={ActivateAccount} path="/register/activate/:slug" exact />
-            <PublicRoute restricted={false} component={SendActivationPage} path="/resend/" exact />
-            <PublicRoute restricted={false} component={SendActivationPage} path="/resend/:slug" exact />
+            <PublicRoute
+                restricted={false}
+                component={LandingPage}
+                path="/"
+                exact
+            />
+            <PublicRoute
+                restricted={false}
+                component={LostPasswordPage}
+                path="/lostpass/:slug?"
+                exact
+            />
+            <PublicRoute
+                restricted={false}
+                component={ResetPasswordPage}
+                path="/reset/:slug"
+                exact
+            />
+            <PublicRoute
+                restricted={false}
+                component={ActivateAccount}
+                path="/register/activate/:slug"
+                exact
+            />
+            <PublicRoute
+                restricted={false}
+                component={SendActivationPage}
+                path="/resend/:slug?"
+                exact
+            />
             <PublicRoute restricted component={Login} path="/login" exact />
-            <PublicRoute restricted component={Login} path="/login/:slug" exact />
+            <PublicRoute
+                restricted
+                component={Login}
+                path="/login/:slug?"
+                exact
+            />
             <PrivateRoute component={Logout} path="/logout" exact />
-            <PublicRoute restricted component={Register} path="/register" exact />
+            <PublicRoute
+                restricted
+                component={Register}
+                path="/register"
+                exact
+            />
             <PrivateRoute component={HomePage} path="/home" exact />
             <PrivateRoute component={Dashboard} path="/dashboard" exact />
-            <Route component={NotFound} />
+            <PublicRoute restricted={false} component={NotFound} />
         </Switch>
     </Router>
 );
